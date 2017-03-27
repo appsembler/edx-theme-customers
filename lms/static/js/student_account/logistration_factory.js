60fb924377d3edeac6e9f26bb0df8bede75ba3f1
Bryan Wilson <bryanlandia@gmail.com>
2017-03-27 14:43:59 +0800

    Call select2() on district dropdown after render

---
+++ lms/static/js/student_account/logistration_factory.js
@@ -0,0 +1,15 @@
+;(function (define) {
+    'use strict';
+    define([
+            'jquery',
+            'eucalyptus-theme-codebase/js/student_account/views/AccessView'
+        ],
+        function($, AccessView) {
+            return function(options) {
+                var $logistrationElement = $('#login-and-registration-container');
+
+                new AccessView(_.extend(options, {el: $logistrationElement}));
+            };
+        }
+    );
+}).call(this, define || RequireJS.define);
